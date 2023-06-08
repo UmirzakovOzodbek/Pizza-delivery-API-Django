d7b9d2a94aa562af90d27eeb94efc092cc8286d1
UmirzakovOzodbek <122611764+umirzakovozodbek@users.noreply.github.com>
2023-06-08 18:01:43 +0500

    Initial commit

---
+++ orders/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'flavour', 'size', 'order_status', 'size']
+    list_filter = ['placed_at', 'updated_at', 'order_status']
